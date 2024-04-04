c17521f12becbbc4fa9e0fe0ee2aa7a30dc94b3c
Dio <dionissarodrigo@gmail.com>
2024-04-04 14:28:32 -0300

    First Commit

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.picpaysimplificado.picpaysimplificado.domain.user;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
